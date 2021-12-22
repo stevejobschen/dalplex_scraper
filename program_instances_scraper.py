2ffef9caa7bca66003741a4a30b288e745e3e0d9
Steve Chen <stevejobschen@icloud.com>
2021-12-22 01:55:01 -0400

    Adding scraper for date and time

--- program_instances_scraper.py
+++ program_instances_scraper.py
@@ -1,4 +1,4 @@
-from urllib.request import urlopen as uReq
+from urllib.request import DataHandler, urlopen as uReq
 from bs4 import BeautifulSoup as soup
 
 def CsvWriterStart(filename,headers):
@@ -18,3 +18,22 @@ def CsvWriter(contentDictionary):
         f.write(i + ";" + courseId+";"+semesterId+";"+"\n")
     CsvWriterStop(f)
 
+rootUrl = "https://www.dalsports.dal.ca/Program/GetProgramDetails?courseId=8993d840-c85b-4afb-b8a9-3c30b3c16817&semesterId=aba777e9-06a5-4d2b-a093-8ba8d8153881#"
+uClient = uReq(rootUrl)
+page_html = uClient.read()
+uClient.close()
+
+page_soup = soup(page_html, "html.parser")
+categorys = page_soup.findAll("div", {"class": "col-sm-6 col-md-4 program-schedule-card"})
+
+linkDictionary = {}
+
+for category in categorys:
+    date = category.find("label", {"class": "program-schedule-card-header"}).get_text().strip()
+    time = category.find("small").get_text().strip()
+    print(time)
+    # title = category.find("h3", {"class": "TitleText-SP"})
+    # linkDictionary.update({title.get_text():link.get('onclick').split("?",1)[1]})
+
+#save to CSV
+# CsvWriter(linkDictionary)
